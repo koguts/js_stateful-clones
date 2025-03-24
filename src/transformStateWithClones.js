d40752429c36490b6cff51d3ffc957ad0e20568c
Sofiia Kogut <kogutsofia288@gmail.com>
2025-03-24 16:35:25 +0200

    add a solution 2

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,27 +8,29 @@
  */
 
 function transformStateWithClones(state, actions) {
-  // write code here
   const result = [];
   let currentState = { ...state };
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      currentState = { ...currentState, ...action.extraData };
+    switch (action.type) {
+      case 'addProperties':
+        currentState = { ...currentState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        currentState = { ...currentState };
+
+        for (const key of action.keysToRemove) {
+          delete currentState[key];
+        }
+        break;
+
+      case 'clear':
+        currentState = {};
+        break;
     }
 
-    if (action.type === 'removeProperties') {
-      currentState = { ...currentState };
-
-      for (const key of action.keysToRemove) {
-        delete currentState[key];
-      }
-    }
-
-    if (action.type === 'clear') {
-      currentState = {};
-    }
-    result.push(currentState);
+    result.push({ ...currentState }); // Робимо копію перед додаванням у масив
   }
 
   return result;
